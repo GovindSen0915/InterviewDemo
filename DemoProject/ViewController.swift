0c336f6de4e4d40b0ad285d412df15c81b857b0b
Govind Sen <govindsen0915@gmail.com>
2025-04-03 18:45:04 +0530

    UI changes

--- DemoProject/ViewController.swift
+++ DemoProject/ViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var myTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        myTextField.layer.cornerRadius = 8
+        myTextField.layer.masksToBounds = true
     }
 
 
